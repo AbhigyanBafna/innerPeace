b150ad6b03b0624d3045e087d9de735cc8cafb3d
AbhigyanBafna <101444239+AbhigyanBafna@users.noreply.github.com>
2023-04-06 20:27:16 +0530

    Initial Commit

---
+++ innerPeace/innerPeaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  innerPeaceApp.swift
+//  innerPeace
+//
+//  Created by Abhigyan Bafna on 06/04/23.
+//
+
+import SwiftUI
+
+@main
+struct innerPeaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
